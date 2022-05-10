f05560577bfaed7bb5cebd60c623bd458f6f01f5
dhainara <daynaralinnyker@gmail.com>
2022-05-09 22:37:14 -0300

    update port ver: 2.0

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
-const port = processs.env.PORT|| 3001
+const port = 3001
 const __dirname = path.resolve(path.dirname(''))
 
 console.log(`Path de ${__dirname}.`)
